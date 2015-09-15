3434cede4fa57ffb030ae2fa2607520784881c4c
Michael Cavalea <callmecavs@gmail.com>
2015-09-15 00:34:23 -0400

    revert previous

--- src/layzr.js
+++ src/layzr.js
@@ -1,26 +1,7 @@
 'use strict'
 
 export default class Layzr {
-  // TODO: more default options/destructuring?
-  constructor(options = {}) {
-    this.container = document.querySelector(options.container) || window
-    this.selector  = options.selector || '[data-layzr]'
-  }
-
-  run(selector = this.selector) {
-    // selector is the same -> replace existing elements
-    // selector is different -> add to existing elements
-    selector === this.selector
-      ? this.elements = this._getElements(selector)
-      : this.elements = this.elements.concat(this._getElements(selector))
-  }
-
-  _getElements(selector) {
-    // use qSA based on container to minize # of elements checked
-    const elements = this.container === window
-      ? document.querySelectorAll(selector)
-      : this.container.querySelectorAll(selector)
-
-    return Array.prototype.slice.call(elements)
+  constructor(options) {
+    console.log(this)
   }
 }
