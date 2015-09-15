69b41f194c9fe37db34ab67897510abff02cdf80
Michael Cavalea <callmecavs@gmail.com>
2015-09-14 20:55:57 -0400

    add run method and _getElements helper

--- dist/layzr.js
+++ dist/layzr.js
@@ -19,13 +19,43 @@
 })(this, function (exports, module) {
   'use strict';
 
+  var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
   function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-  var Layzr = function Layzr(options) {
-    _classCallCheck(this, Layzr);
+  var Layzr = (function () {
+    // TODO: more default options/destructuring?
 
-    console.log(this);
-  };
+    function Layzr() {
+      var options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+      _classCallCheck(this, Layzr);
+
+      this.container = document.querySelector(options.container) || window;
+      this.selector = options.selector || '[data-layzr]';
+    }
+
+    _createClass(Layzr, [{
+      key: 'run',
+      value: function run() {
+        var selector = arguments.length <= 0 || arguments[0] === undefined ? this.selector : arguments[0];
+
+        // selector is the same -> replace existing elements
+        // selector is different -> add to existing elements
+        selector === this.selector ? this.elements = this._getElements(selector) : this.elements = this.elements.concat(this._getElements(selector));
+      }
+    }, {
+      key: '_getElements',
+      value: function _getElements(selector) {
+        // use qSA based on container to minize # of elements checked
+        var elements = this.container === window ? document.querySelectorAll(selector) : this.container.querySelectorAll(selector);
+
+        return Array.prototype.slice.call(elements);
+      }
+    }]);
+
+    return Layzr;
+  })();
 
   module.exports = Layzr;
 });
--- src/layzr.js
+++ src/layzr.js
@@ -1,7 +1,26 @@
 'use strict'
 
 export default class Layzr {
-  constructor(options) {
-    console.log(this)
+  // TODO: more default options/destructuring?
+  constructor(options = {}) {
+    this.container = document.querySelector(options.container) || window
+    this.selector  = options.selector || '[data-layzr]'
+  }
+
+  run(selector = this.selector) {
+    // selector is the same -> replace existing elements
+    // selector is different -> add to existing elements
+    selector === this.selector
+      ? this.elements = this._getElements(selector)
+      : this.elements = this.elements.concat(this._getElements(selector))
+  }
+
+  _getElements(selector) {
+    // use qSA based on container to minize # of elements checked
+    const elements = this.container === window
+      ? document.querySelectorAll(selector)
+      : this.container.querySelectorAll(selector)
+
+    return Array.prototype.slice.call(elements)
   }
 }
